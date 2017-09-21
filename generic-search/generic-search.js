6f2f89dec337455a67fa6b65665364ae98e10f4f
adrianrosa93 <adrianrosa93@gmail.com>
2017-09-21 11:47:41 -0300

    Add limit and offset to paginate

--- generic-search/generic-search.js
+++ generic-search/generic-search.js
@@ -1,31 +1,44 @@
-function executeSearch(recordType, filtersObj, columnsObj, limit){
+/* Search pagination with limit and offset for any record.
+ * The result data will be returned into an array of objects. The properties from each object will be determined by the param 'columnsObj'.
+ * @param {string} recordType: type of the record to search
+ * @param {array} filtersObj: array of objects with filters
+ * @param {array} columnsObj: array of objects with columns
+ * @param {int} limit: by default is 1000
+ * @param {int} offset: by default is 0
+ * @returns {array}
+*/
+function executeSearch(recordType, filtersObj, columnsObj, limit, offset){
 
     var filters = getFilters(filtersObj);
     var columns = getColumns(columnsObj);
     var search = nlapiCreateSearch(recordType, filters, columns);
     var resultset = search.runSearch();
-    var searchid = 0;
-    var columnsResults = search.getColumns();
+    var searchid = offset ? offset : 0;
+    var searchLimit = (limit && limit < 1000) ? (limit + searchid) : 1000;
     var dataResult = [];
 
     do {
 
-        var resultslice = resultset.getResults(searchid, limit < 1000 ? limit : searchid + 1000);
+        var resultslice = resultset.getResults(searchid, searchLimit);
 
         for (var rs in resultslice) {
 
-            dataResult.push(getObjectResult(resultslice[rs], columnsObj, columnsResults));		
+            dataResult.push(getObjectResult(resultslice[rs], columnsObj, columns));
 
            	yieldScript(nlapiGetContext());
 
         	searchid++;
         }
 
-    } while (resultslice.length >= 1000 && searchid < limit);
+    } while (searchid < searchLimit);
 
     return dataResult;
 }
 
+/* Return an array with the Netsuite columns object (nlobjSearchColumn).
+ * @param {array} columnsObj: objects with the column data
+ * @returns {array}
+*/
 function getColumns(columnsObj){
 	
 	var columns = new Array(),
@@ -47,6 +60,10 @@ function getColumns(columnsObj){
 	return columns;
 }
 
+/* Return an array with the Netsuite filters object (nlobjFilterColumn).
+ * @param {array} filtersObj: objects with the filter data
+ * @returns {array}
+*/
 function getFilters(filtersObj){
 	
 	var filters = new Array(),
@@ -65,6 +82,12 @@ function getFilters(filtersObj){
 	return filters;
 }
 
+/* Return an object with the result data mapped by property 'display' from the param 'columns'.
+ * @param {nlobjSearchResultSet} obj: this object contains the result data
+ * @param {array} columns: of objects, is required for the property 'display'
+ * @param {array} columnsNS: of nlobjSearchColumn
+ * @returns {object}
+*/
 function getObjectResult(obj, columns, columnsNS){
     
    var objResult = {};
@@ -77,6 +100,10 @@ function getObjectResult(obj, columns, columnsNS){
    return objResult;
 }
 
+/* If the script needs to reload process units (less than 1000) this method will do it.
+ * @param {nlobjContext} context: object to check units remaining
+ * @returns {undefined}
+*/
 function yieldScript(context) {
 
     if (context.getRemainingUsage() < 1000) {
